dfa38a525feaa7a509c5c111036c09b47c54f955
ANIKETBOMBLE <aniketbomble999@gmail.com>
2025-04-21 23:51:58 +0530

    :new:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React  from "react";
+import React, { use }  from "react";
+import instance from "./axios";
 
-function Home(){
+
+function Main() {
+	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
+  
+	useEffect(() => {
+	  getData();
+	}, []);
+  
+	async function getData() {
+	  setLoading(true);
+	  const result = await instance.get("/products");
+	  // console.log(result.data.products);
+	  setProducts(result.data.products);
+	  setLoading(false);
+	}
+	
+	
+	
+	
+	
 	return(
 		<>
 		<h2>home</h2>
+		
 		</>
 	)
 }
