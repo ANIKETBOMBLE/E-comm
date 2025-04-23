b511670d9a8c4cc49faae0887d80591f3787d3b6
ANIKETBOMBLE <aniketbomble999@gmail.com>
2025-04-24 01:12:15 +0530

    new

--- src/componetus/F.jsx
+++ src/componetus/F.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
 const Footer = () => {
 	return (
 	  <footer className="bg-gray-900 text-white py-10 mt-20">
--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../componetus/Header";
-import Footer from "../componetus/Footer";
+import Header from "../componetus/H.jsx";
+import Footer from "../componetus/F.jsx";
 import { Outlet } from "react-router-dom";
 function First(){
 	return(
